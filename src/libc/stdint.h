e3b28b831e9f34bac3c09b265a74f70b805caa00
renanrod <renan2.rodrigues@usp.br>
2016-11-12 15:35:16 -0200

    Add files via upload

---
+++ src/libc/stdint.h
@@ -0,0 +1,46 @@
+#ifndef _INTEIRO_H_
+#define _INTEIRO_H_
+
+#include <math.h>
+
+#define INTMAX_MIN (- (pow(2,31) - 1) )
+
+#define INTMAX_MAX (pow(2,31) - 1)
+
+#define UINTMAX_MAX (pow(2,32) - 1)
+
+#define INTPTR_MIN (- (pow(2,15) - 1) )
+
+#define INTPTR_MAX (pow(2,15) - 1)
+
+#define UINTPTR_MAX (pow(2,16) - 1)
+
+#define INTN_MIN (- (pow(2,31)) )
+
+#define INTN_MAX (pow(2,31) - 1)
+
+#define UINTN_MAX (pow(2,32) - 1)
+
+#define INT_LEASTN_MIN (- (pow(2,31) - 1) )
+
+#define INT_LEASTN_MAX (pow(2,31) - 1)
+
+#define UINT_LEASTN_MAX (pow(2,32) - 1)
+
+#define INT_FASTN_MIN (- (pow(2,31) - 1) )
+
+#define INT_FASTN_MAX (pow(2,32) - 1)
+
+#define UINT_FASTN_MAX (pow(2,32) - 1)
+
+#define SIZE_MAX (pow(2,32) -1 )
+
+#define PTRDIFF_MIN (- (pow(2,16) - 1) )
+
+#define PTRDIFF_MAX (pow(2,16) - 1)
+
+
+
+
+
+#endif
