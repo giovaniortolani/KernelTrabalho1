84d030e322d818b09791d35011d03f6d360c8985
Gustavo Aguiar <gugahoa@gmail.com>
2016-11-12 17:34:48 -0200

    printNumber works for any base (but please only use 2, 10, 16)

--- src/libc/stdio.c
+++ src/libc/stdio.c
@@ -2,7 +2,7 @@
 #include "../video.h"
 
 void printNumber(int number, int base) {
-	int base_port = 1;
+	int base_pot = 1;
 
 	if (number < 0) {
 		video_putChar('-');
@@ -10,18 +10,22 @@ void printNumber(int number, int base) {
 	}
 
 	do {
-		base_port *= base;
-	} while (base_port < number);
-	base_port /= base;
+		base_pot *= base;
+	} while (base_pot <= number);
+	base_pot /= base;
 
 	int rest = 0;
 	int put_num = 0;
-	while (base_port != 0) {
-		rest = number % base_port;
-		put_num = (number - rest) / base_port;
-		video_putChar('0' + put_num);
+	while (base_pot != 0) {
+		rest = number % base_pot;
+		put_num = (number - rest) / base_pot;
+		if (put_num < 10) {
+			video_putChar('0' + put_num);
+		} else {
+			video_putChar((put_num - 10) + 'A');
+		}
 
 		number = rest;
-		base_port /= base;
+		base_pot /= base;
 	}
 }
